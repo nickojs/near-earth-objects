a370009cbe3e1cda9d9c3a3b88d5cfe479e3ef2b
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-03 14:52:53 -0300

    Initial styles for search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -6,25 +6,34 @@ const Search = () => {
   const [finalDate, setFinalDate] = useState('');
 
   return (
-    <div>
-      <h1>Search parameters</h1>
-      <input
-        type="date"
-        name="initialDate"
-        onChange={(event) => setInitialDate(event.target.value)}
-      />
-      <input
-        type="date"
-        name="endDate"
-        min={initialDate}
-        onChange={(event) => setFinalDate(event.target.value)}
-      />
-      <button
-        type="button"
-        disabled={initialDate.length <= 0 || finalDate.length <= 0}
-      >Go
-      </button>
-    </div>
+    <S.Container>
+      <S.Title>Search parameters</S.Title>
+      <S.Inputs>
+        <S.InputWrapper>
+          <label htmlFor="initialDate">Initial Date</label>
+          <input
+            type="date"
+            name="initialDate"
+            onChange={(event) => setInitialDate(event.target.value)}
+          />
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <label htmlFor="finalDate">Final Date</label>
+          <input
+            type="date"
+            name="finalDate"
+            min={initialDate}
+            onChange={(event) => setFinalDate(event.target.value)}
+          />
+        </S.InputWrapper>
+        <S.SearchButton
+          type="button"
+          disabled={initialDate.length <= 0 || finalDate.length <= 0}
+        >Search
+        </S.SearchButton>
+
+      </S.Inputs>
+    </S.Container>
   );
 };
 
--- src/components/search/styles.js
+++ src/components/search/styles.js
@@ -1,1 +1,50 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  color: ${({ theme }) => theme.color};
+  background: ${({ theme }) => theme.dark};
+`;
+
+export const Title = styled.div`
+  display: block;
+  width: 100%;
+  min-width: 100%;
+  padding: 12px;
+
+  background: ${({ theme }) => theme.light};
+`;
+
+export const SearchButton = styled.button`
+  padding: 6px 24px;
+  border: 2px solid lightblue;
+  box-shadow: 0 0 10px lightblue;
+  font-weight: bold;
+  color: white;
+  cursor: pointer;
+  transition: .5s ease;
+
+  :hover{ 
+    background: white;
+    color: lightblue;
+  }
+
+  :active{ 
+    transform: translateX(-5px);
+  }
+`;
+
+export const Inputs = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-end;
+  padding: 12px;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  
+  input { margin-top: 6px; }
+`;
