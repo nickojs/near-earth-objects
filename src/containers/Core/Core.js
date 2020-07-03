abea1d1767a1e267b43e94e338507af2e8c69bd6
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-03 17:22:42 -0300

    Added API key to the project, importing it on Core. In a real life application, this should not be done

--- src/containers/Core/Core.js
+++ src/containers/Core/Core.js
@@ -1,29 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Search from '../../components/search/search';
 import useRequest from '../../hooks/request';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 const Core = () => {
   const [options, setOptions] = useState(null);
-  const { data, loading, error } = useRequest(options);
+  const [requestData] = useRequest(options);
+  const { data, loading, error } = requestData;
 
   const optionsHandler = (query) => {
     const values = {
       method: 'GET',
-      url: `https://api.nasa.gov/neo/rest/v1/${query}&api_key=API_KEY`
+      url: `https://api.nasa.gov/neo/rest/v1/${query}&api_key=${API_KEY}`
     };
     setOptions(values);
   };
 
-  useEffect(() => {
-    // demo to check for request values
-    console.log('data: ', data);
-    console.log('loading: ', loading);
-    console.log('error: ', error);
-  }, [data, loading, error]);
-
   return (
     <div>
       <Search setQuery={optionsHandler} />
+      {loading && <p>loading...</p>}
+      {error && <p>error: code: {error.error.code} message: {error.error.code} </p>}
     </div>
   );
 };
