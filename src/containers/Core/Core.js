05504f4ba0459326b542861e7c17b44e86f6140d
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-04 19:28:21 -0300

    Using mocked Neolist on Core, adjusted API_KEY variable

--- src/containers/Core/Core.js
+++ src/containers/Core/Core.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import Search from '../../components/search/search';
 import NeoList from '../../components/NeoList/NeoList';
-import Loading from '../../components/loading/loading';
-import Dialog from '../../components/dialog/dialog';
+import Status from '../../components/status/status';
 
 import useRequest from '../../hooks/request';
-const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Core = () => {
   const [options, setOptions] = useState(null);
@@ -15,7 +13,7 @@ const Core = () => {
   const optionsHandler = (query) => {
     const values = {
       method: 'GET',
-      url: `https://api.nasa.gov/neo/rest/v1/${query}&api_key=${API_KEY}`
+      url: `https://api.nasa.gov/neo/rest/v1/${query}&api_key=${process.env.REACT_APP_API_KEY}`
     };
     setOptions(values);
   };
@@ -33,20 +31,9 @@ const Core = () => {
   return (
     <div>
       <Search setQuery={optionsHandler} />
-      {loading && (
-        <Dialog>
-          <p>loading...</p>
-          <Loading />
-        </Dialog>
-      )}
-      {error && (
-        <Dialog type="danger">
-          <p>Oops, something went wrong!</p>
-          <br />
-          <p>{error.error.code}</p>
-        </Dialog>
-      )}
+      {/* {neoList} */}
       <NeoList title="2020-07-04" />
+      <Status loading={loading} error={error} />
     </div>
   );
 };
