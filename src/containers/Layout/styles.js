7d35c0a14fc257e55ba3eceadf58d921372fe605
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-04 17:36:34 -0300

    Removed background from Layout

--- src/containers/Layout/styles.js
+++ src/containers/Layout/styles.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
-import bg from '../../assets/image/background.jpg';
 
 export const Container = styled.div`
   max-width: 1200px; 
   height: 100vh;
   margin: 0 auto;
-  background: url(${bg});
 `;
