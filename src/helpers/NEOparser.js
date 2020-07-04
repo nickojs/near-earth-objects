2460f2d6601c2461f0a628e8f1b8435f7bdeb455
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-04 19:56:18 -0300

    Created parser to obviously parse the API object

---
+++ src/helpers/NEOparser.js
@@ -0,0 +1,37 @@
+export default (neo) => {
+  const preview = {
+    id: neo.id,
+    name: neo.name,
+    url: neo.url,
+
+    magnitude: neo.absolute_magnitude_h,
+    hazardous: neo.is_potentially_hazardous_asteroid,
+    sentry: neo.is_sentry_object,
+    orbiting: neo.orbiting_body,
+
+    diameter: {
+      kilometers: {
+        min: neo.estimated_diameter.kilometers.estimated_diameter_min,
+        max: neo.estimated_diameter.kilometers.estimated_diameter_max
+      },
+      meters: {
+        min: neo.estimated_diameter.meters.estimated_diameter_min,
+        max: neo.estimated_diameter.meters.estimated_diameter_max
+      },
+      miles: {
+        min: neo.estimated_diameter.miles.estimated_diameter_min,
+        max: neo.estimated_diameter.miles.estimated_diameter_max
+      },
+      feet: {
+        min: neo.estimated_diameter.feet.estimated_diameter_min,
+        max: neo.estimated_diameter.feet.estimated_diameter_max
+      }
+    }
+  };
+
+  const details = {
+
+  };
+
+  return [preview, details];
+};
