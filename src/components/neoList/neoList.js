c54467b18fc086efa5496f5a03dd20007d7f5cb3
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-04 21:01:44 -0300

    Using new parser helper

--- src/components/neoList/neoList.js
+++ src/components/neoList/neoList.js
@@ -3,17 +3,16 @@ import Neo from './neo/neo';
 import { Title, Container } from '../../generalStyles';
 import * as S from './styles';
 import example from './neo/example.json';
-import parser from '../../helpers/NEOparser';
+import { preview } from '../../helpers/NEOparser';
 
 const NeoList = ({ title, list }) => {
   // 'NEO' stands for 'Near Earth Objects'
   // 'list' is a set of NEOs for a given date, i.e:
 
   const neos = example.map((each) => {
-    const parsedObj = parser(each);
-    const [preview] = parsedObj;
+    const parsedObj = preview(each);
     console.log(preview);
-    return <Neo key={each.id} object={preview} />;
+    return <Neo key={each.id} object={parsedObj} />;
   });
 
   return (
