b6b4094a971a5905fee708dc4c744c49f85a7dc3
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-05 16:46:54 -0300

    Added parser to details object

--- src/helpers/NEOparser.js
+++ src/helpers/NEOparser.js
@@ -27,3 +27,92 @@ export const preview = (neo) => ({
     }
   }
 });
+
+const closeApproachParser = (data) => ({
+  date: data.close_approach_date_full,
+  epoch: data.epoch_date_close_approach,
+  velocity: {
+    kmSecond: data.relative_velocity.kilometers_per_second,
+    kmHour: data.relative_velocity.kilometers_per_hour,
+    milesHour: data.relative_velocity.miles_per_hour
+  },
+  miss: {
+    astronomical: data.miss_distance.astronomical,
+    lunar: data.miss_distance.lunar,
+    kilometers: data.miss_distance.kilometers,
+    miles: data.miss_distance.miles
+  },
+  orbiting: data.orbiting_body
+});
+
+const orbitalDataParser = (data) => ({
+  general: {
+    orbitId: data.orbit_id,
+    firstObservation: data.first_observation_date,
+    lastObservation: data.last_observation_date,
+    dataArc: data.data_arc_in_days
+  },
+  info: {
+    jupiterTisserand: {
+      value: data.jupiter_tisserand_invariant,
+      unit: null
+    },
+    eccentricity: {
+      value: data.eccentricity,
+      unit: null
+    },
+    majorAxis: {
+      value: data.semi_major_axis,
+      unit: 'au'
+    },
+    inclination: {
+      value: data.inclination,
+      unit: 'deg'
+    },
+    ascendingNode: {
+      value: data.ascending_node_longitude,
+      unit: 'deg'
+    },
+    perihelionArgument: {
+      value: data.perihelion_argument,
+      unit: 'deg'
+    },
+    perihelionTime: {
+      value: data.perihelion_time,
+      unit: 'TDB'
+    },
+    meanAnomaly: {
+      value: data.mean_anomaly,
+      unit: 'deg'
+    },
+    meanMotion: {
+      value: data.mean_motion,
+      unit: 'deg/d'
+    },
+    orbitPeriod: {
+      value: data.orbital_period,
+      unit: 'd'
+    },
+    equinox: {
+      value: data.equinox,
+      unit: null
+    }
+  },
+  class: {
+    type: data.orbit_class.orbit_class_type,
+    range: data.orbit_class.orbit_class_range,
+    description: data.orbit_class.orbit_class_description
+  }
+});
+
+export const details = (neo) => {
+  // eslint-disable-next-line camelcase
+  const { close_approach_data: CPA, orbital_data: orbital } = neo;
+  const parsedCPA = CPA.map((each) => closeApproachParser(each));
+  const parsedOrbital = orbitalDataParser(orbital);
+
+  return {
+    closeApproachData: parsedCPA,
+    orbitalData: parsedOrbital
+  };
+};
