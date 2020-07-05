9d15c2b4877c7c1022ea89521fed27123c1d0eaa
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-04 21:01:10 -0300

    Modularazing NEOParser to parse more object types

--- src/helpers/NEOparser.js
+++ src/helpers/NEOparser.js
@@ -1,37 +1,29 @@
-export default (neo) => {
-  const preview = {
-    id: neo.id,
-    name: neo.name,
-    url: neo.nasa_jpl_url,
+export const preview = (neo) => ({
+  id: neo.id,
+  name: neo.name,
+  url: neo.nasa_jpl_url,
 
-    magnitude: neo.absolute_magnitude_h,
-    hazardous: neo.is_potentially_hazardous_asteroid,
-    sentry: neo.is_sentry_object,
-    orbiting: neo.close_approach_data[0].orbiting_body,
+  magnitude: neo.absolute_magnitude_h,
+  hazardous: neo.is_potentially_hazardous_asteroid,
+  sentry: neo.is_sentry_object,
+  orbiting: neo.close_approach_data[0].orbiting_body,
 
-    diameter: {
-      kilometers: {
-        min: neo.estimated_diameter.kilometers.estimated_diameter_min,
-        max: neo.estimated_diameter.kilometers.estimated_diameter_max
-      },
-      meters: {
-        min: neo.estimated_diameter.meters.estimated_diameter_min,
-        max: neo.estimated_diameter.meters.estimated_diameter_max
-      },
-      miles: {
-        min: neo.estimated_diameter.miles.estimated_diameter_min,
-        max: neo.estimated_diameter.miles.estimated_diameter_max
-      },
-      feet: {
-        min: neo.estimated_diameter.feet.estimated_diameter_min,
-        max: neo.estimated_diameter.feet.estimated_diameter_max
-      }
+  diameter: {
+    kilometers: {
+      min: neo.estimated_diameter.kilometers.estimated_diameter_min,
+      max: neo.estimated_diameter.kilometers.estimated_diameter_max
+    },
+    meters: {
+      min: neo.estimated_diameter.meters.estimated_diameter_min,
+      max: neo.estimated_diameter.meters.estimated_diameter_max
+    },
+    miles: {
+      min: neo.estimated_diameter.miles.estimated_diameter_min,
+      max: neo.estimated_diameter.miles.estimated_diameter_max
+    },
+    feet: {
+      min: neo.estimated_diameter.feet.estimated_diameter_min,
+      max: neo.estimated_diameter.feet.estimated_diameter_max
     }
-  };
-
-  const details = {
-
-  };
-
-  return [preview, details];
-};
+  }
+});
