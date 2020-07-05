d32ab7e93f47819be430c63895fd25e8877733f6
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-05 19:08:44 -0300

    Implemented navbar on Layout

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -3,12 +3,14 @@ import themes from './themes.json';
 
 import * as S from './styles';
 import Theme from '../../Theme';
+import Navbar from '../../components/navbar/navbar';
 
 const Layout = ({ children }) => {
   const currentTheme = themes.standard;
 
   return (
     <Theme theme={currentTheme}>
+      <Navbar />
       <S.Container>
         <S.Title>Near Earth Objects Analyzer</S.Title>
         {children}
