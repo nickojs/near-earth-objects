d8c0cd40f99cc76b0f7f4eb640c04b0a9805efa6
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-05 21:26:58 -0300

    Retrieving store data on Collection

--- src/containers/Collection/Collection.js
+++ src/containers/Collection/Collection.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import NeoList from '../../components/neoList/neoList';
 import { ExternalTitle } from '../../generalStyles';
 
 const Collection = () => {
+  const { collection } = useSelector((state) => state);
   const title = 'my collection';
 
   return (
     <div>
       <ExternalTitle>Collections</ExternalTitle>
-      <NeoList title={title} list={[]} />
+      <NeoList title={title} list={collection} />
     </div>
   );
 };
