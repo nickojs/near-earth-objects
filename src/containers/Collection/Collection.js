348b1d65fab98523d8c91965cc960fd5d1a0484e
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-05 22:14:55 -0300

    Collection now renders individual Neos instead of neoList

--- src/containers/Collection/Collection.js
+++ src/containers/Collection/Collection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import NeoList from '../../components/neoList/neoList';
-import { ExternalTitle } from '../../generalStyles';
+import Neo from '../../components/neoList/neo/neo';
+import { ExternalTitle, Container } from '../../generalStyles';
 
 const Collection = () => {
   const { collection } = useSelector((state) => state);
@@ -10,7 +10,9 @@ const Collection = () => {
   return (
     <div>
       <ExternalTitle>Collections</ExternalTitle>
-      <NeoList title={title} list={collection} />
+      <Container>
+        {collection.map((each) => <Neo object={each} />)}
+      </Container>
     </div>
   );
 };
