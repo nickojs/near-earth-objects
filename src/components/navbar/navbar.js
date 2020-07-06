a8073a57d1886199682ba6f8269a5cf9281aac8c
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-06 11:33:44 -0300

    Implemented themeSelector on navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
+import ThemeSelector from '../themeSelector/themeSelector';
 
-const Navbar = () => (
-  <header>
-    <nav>
-      <S.Navbar>
-        <li>
-          <S.Link to="/">Home</S.Link>
-        </li>
-        <li>
-          <S.Link to="/collection">My Collection</S.Link>
-        </li>
-        <li>Themes</li>
-      </S.Navbar>
-    </nav>
-  </header>
-);
+const Navbar = () => {
+  const [themes, setThemes] = useState(false);
+
+  const themesHandler = () => setThemes(!themes);
+
+  return (
+    <>
+      <header>
+        <nav>
+          <S.Navbar>
+            <li>
+              <S.Link to="/">Home</S.Link>
+            </li>
+            <li>
+              <S.Link to="/collection">My Collection</S.Link>
+            </li>
+            <li onClick={themesHandler}>Themes</li>
+          </S.Navbar>
+        </nav>
+      </header>
+      <ThemeSelector show={themes} clicked={themesHandler} />
+    </>
+  );
+};
 
 export default Navbar;
