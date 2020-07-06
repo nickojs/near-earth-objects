016ecc8a5a267e3102ba8d21dff9fa983e13c508
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-06 11:33:35 -0300

    Created selector to select themes

---
+++ src/components/themeSelector/styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const ThemeContainer = styled.div`
+  position: absolute;
+  top: 0; left: 0; right: 0;
+  height: 100vh;
+  z-index: 2;
+  ${({ show }) => (show ? 'display: block;' : 'display: none;')}
+`;
+
+export const ThemeList = styled.ul`
+  width: 20%;
+  height: 100%;
+
+  box-shadow: 0 0 0 1000px rgba(0, 0, 0, .6);
+
+  color: white;
+  background: black;
+`;
+
+export const ThemeOption = styled.li`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  
+  margin: 0 auto;
+  padding: 12px;
+  cursor: pointer;
+
+  :hover{ 
+    background: white;
+  }
+`;
+
+export const PreviewContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: auto;
+  width: 100px;
+`;
+
+export const PreviewColor = styled.div`
+  width: 20px; height: 20px; margin: 0 2px;
+  background: ${({ color }) => color};
+`;
---
+++ src/components/themeSelector/themeSelector.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import themes from '../../containers/Layout/themes.json';
+import * as S from './styles';
+
+const ThemeSelector = ({ show, clicked }) => {
+  const themeKeys = Object.keys(themes);
+
+  return (
+    <S.ThemeContainer show={show} onClick={clicked}>
+      <S.ThemeList>
+        {themeKeys.map((theme) => (
+          <S.ThemeOption key={theme}>
+            <p>{theme}</p>
+            <S.PreviewContainer>
+              <S.PreviewColor color={themes[theme].dark} />
+              <S.PreviewColor color={themes[theme].light} />
+            </S.PreviewContainer>
+          </S.ThemeOption>
+        ))}
+      </S.ThemeList>
+    </S.ThemeContainer>
+  );
+};
+
+export default ThemeSelector;
